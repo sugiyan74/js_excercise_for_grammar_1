74d897d29883131597fa2c4fbb058536ed8dc31a
ksuigyama <k.sugiyama@apassist.com>
2019-09-07 18:41:08 +0900

    Created an assignment for beginner of html part 2

---
+++ main.js
@@ -0,0 +1,24 @@
+let lastName = '山田';
+let firstName = '太郎';
+let fullName = lastName + ' ' + firstName;
+let myAge = 10;
+let friendAge = 13;
+let totalAge = myAge + friendAge;
+const yearOfDays = 365;
+let numberOfDays = totalAge * yearOfDays;
+let numberOfChildren = 3;
+let numberOfCakes = 10;
+let numberOfCakesByChild = numberOfCakes/numberOfChildren;
+let numberOfRestOfCakes = numberOfCakes%numberOfChildren;
+
+console.log(lastName);
+console.log(firstName);
+console.log(fullName);
+console.log(myAge);
+console.log(friendAge);
+console.log(totalAge);
+console.log(numberOfDays);
+console.log(numberOfChildren);
+console.log(numberOfCakes);
+console.log(numberOfCakesByChild);
+console.log(numberOfRestOfCakes);
